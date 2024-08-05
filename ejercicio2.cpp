0c64e84291b974556d6cd244c5e3a447b5f6b76d
nmenesesmed <nmenesesmed@unjbg.edu.pe>
2024-08-05 09:58:47 -0500

    parte 2
    Funcion para Mostrar Atletas

--- ejercicio2.cpp
+++ ejercicio2.cpp
@@ -7,3 +7,12 @@ struct Atleta {
     string pais;
     int medallas;
 };
+
+void mostrarAtletas(const Atleta atletas[], int numAtletas) {
+    for (int i = 0; i < numAtletas; ++i) {
+        cout << "Nombre: " << atletas[i].nombre << endl;
+        cout << "Pais: " << atletas[i].pais << endl;
+        cout << "Medallas: " << atletas[i].medallas << endl;
+        cout << "-------------------------\n";
+    }
+}
