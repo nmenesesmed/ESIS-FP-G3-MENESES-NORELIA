a0c13197a72c68a09491b8c1b0f7a826d17723f5
nmenesesmed <nmenesesmed@unjbg.edu.pe>
2024-08-05 09:51:32 -0500

    ejercicio1.2
    Ubicando la matriz de n*3n

--- ejercicio1.cpp
+++ ejercicio1.cpp
@@ -7,3 +7,29 @@ int main() {
     int filas = n;
     int columnas = 3 * n;
     int matriz[filas][columnas];
+    
+    for (int i = 0; i < filas; ++i) {
+        for (int j = 0; j < columnas; ++j) {
+            matriz[i][j] = 0;
+        }
+    }
+
+    for (int j = 0; j < columnas; ++j) {
+        matriz[0][j] = 1;
+        matriz[filas - 1][j] = 1;
+    }
+
+    for (int i = 0; i < filas; ++i) {
+        matriz[i][0] = 1;
+        matriz[i][columnas - 1] = 1;
+    }
+
+    for (int i = 0; i < filas; ++i) {
+        for (int j = 0; j < columnas; ++j) {
+            cout << matriz[i][j];
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
