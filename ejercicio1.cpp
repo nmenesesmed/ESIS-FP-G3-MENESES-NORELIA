e2577b7b35cd39d83f78aeef5e4174ab958af47a
nmenesesmed <nmenesesmed@unjbg.edu.pe>
2024-08-05 09:49:18 -0500

    parte 1
    Definicion de variables

---
+++ ejercicio1.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cout << "Ingrese el valor de n: "; cin >> n;
+
+    int filas = n;
+    int columnas = 3 * n;
+    int matriz[filas][columnas];
